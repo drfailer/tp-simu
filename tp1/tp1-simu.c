b48076528d2533c77a860b422b2719a11935db2b
drfailer <drfailer.42@gmail.com>
2022-10-02 15:09:36 +0200

    fin tp1

--- tp1/tp1-simu.c
+++ tp1/tp1-simu.c
@@ -8,7 +8,8 @@
 
 /* GLOBAL VARIABLES: */
 
-static long int g_curRandLCG = 5; // `long int` is required with the smart parameters
+static long int g_curRandLCG =
+    5; // `long int` is required with the smart parameters
 static u_int8_t g_currRandSR = 0b00000110;
 
 /* VON NEUMAN (middle square): */
@@ -19,11 +20,12 @@ void middleSquare(int n0, int it) {
 
   for (i = 0; i < it; ++i) {
     printf("%d\n", n);
-    n = (n*n/100)%10000;
+    n = (n * n / 100) % 10000;
   }
 }
 
 void testMiddleSquare() {
+  printf("\n\n=== Middle Square ===\n\n");
   printf("##### 1234:\n");
   middleSquare(1234, 10);
   printf("##### 4100:\n");
@@ -43,7 +45,7 @@ void coinTossing(int repeate) {
 
   // repeating experience
   for (i = 0; i < repeate; ++i) {
-    r = rand()%2;
+    r = rand() % 2;
     if (r) {
       ++heads;
     } else {
@@ -52,11 +54,12 @@ void coinTossing(int repeate) {
   }
 
   printf("Conclusion with %d:\n", repeate);
-  printf("heads: %f\n", (float) heads/repeate);
-  printf("tails: %f\n", (float) tails/repeate);
+  printf("heads: %f\n", (float)heads / repeate);
+  printf("tails: %f\n", (float)tails / repeate);
 }
 
 void testCoinTossing() {
+  printf("\n\n=== Coin tossing experiment ===\n\n");
   coinTossing(10);
   coinTossing(100);
   coinTossing(1000);
@@ -70,12 +73,12 @@ void rool6(int repeate) {
   int results[6] = {0};
 
   for (i = 0; i < repeate; ++i) {
-    results[rand()%6] += 1;
+    results[rand() % 6] += 1;
   }
   // Affiche:
   printf("6 - Conclusion for %d:\n", repeate);
   for (i = 0; i < 6; ++i) {
-    printf("%d: %f\n", i+1, (float) results[i]/repeate);
+    printf("%d: %f\n", i + 1, (float)results[i] / repeate);
   }
 }
 
@@ -84,20 +87,23 @@ void rool10(int repeate) {
   int results[10] = {0};
 
   for (i = 0; i < repeate; ++i) {
-    results[rand()%10] += 1;
+    results[rand() % 10] += 1;
   }
   // Affiche:
   printf("10 - Conclusion for %d:\n", repeate);
   for (i = 0; i < 10; ++i) {
-    printf("%d: %f\n", i+1, (float) results[i]/repeate);
+    printf("%d: %f\n", i + 1, (float)results[i] / repeate);
   }
   printf("\n");
 }
 
 void testDice() {
+  printf("\n\n=== dices experiment ===\n\n");
+  printf("\n\n--- dices 6 ---\n\n");
   rool6(10);
   rool6(100);
   rool6(1000);
+  printf("\n\n--- dices 10 ---\n\n");
   rool10(10);
   rool10(100);
   rool10(1000);
@@ -109,35 +115,35 @@ void lcg(int a, int c, int m, int x, int repeate) {
   int i;
 
   for (i = 0; i < repeate; ++i) {
-    x = (a * x + c)%m;
+    x = (a * x + c) % m;
     printf("%d\n", x);
   }
+  printf("\n");
 }
 
 long int intRand() {
-  g_curRandLCG = (A * g_curRandLCG + C)%M;
+  g_curRandLCG = (A * g_curRandLCG + C) % M;
   return g_curRandLCG;
 }
 
-double floatRand() {
-  return ((double) intRand() / M);
-}
+double floatRand() { return ((double)intRand() / M); }
 
 void testLCG() {
   int i;
 
+  printf("\n\n=== LCG ===\n\n");
+
   lcg(5, 1, 16, 5, 32);
-  printf("\n");
 
   // test intRand
-  printf("intRand\n");
+  printf("###### intRand:\n");
   for (i = 0; i < 32; ++i) {
     printf("%ld\n", intRand());
   }
   printf("\n");
 
   // test floatRand
-  printf("floatRand\n");
+  printf("###### floatRand:\n");
   for (i = 0; i < 32; ++i) {
     printf("%f\n", floatRand());
   }
@@ -169,10 +175,11 @@ u_int8_t srg() {
 void testSRG() {
   int i;
 
+  printf("\n\n=== SRG ===\n\n");
+
   for (i = 0; i < 16; ++i) {
     displayBinN(srg());
   }
-
 }
 
 /* MAIN: */
@@ -181,19 +188,28 @@ int main(void) {
   srand(0);
 
   // von neuman
-  /* testMiddleSquare(); */
+  testMiddleSquare();
 
   // coin tossing
-  /* testCoinTossing(); */
+  testCoinTossing();
 
   // dice
-  /* testDice(); */
+  testDice();
 
   // lcg
-  /* testLCG(); */
+  testLCG();
 
   // shift register
-  /* testSRG(); */
+  testSRG();
 
   return 0;
 }
+
+/** QUESTION:
+ * 3. `rand` utilise un générateur congruenciel linéaire qui est à évité dans
+ *    les programmes scientifiques.
+ * 10. Library qui implémente Mersenne twister:
+ *    https://github.com/ESultanik/mtwister
+ * 11. Diehard statistical tests:
+ *    https://github.com/GINARTeam/Diehard-statistical-test
+ */
